a1ee52d83d3a541630c0f8820fc6c9ef53024ad0
rmahfuj-dev <rmahfujur782@gmail.com>
2026-05-04 18:21:26 +0600

    project has been created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    const links = ["Services","International","About","Work","Careers","Blog","Webiner"]
+  return (
+    <nav>
+      <div className="logo"></div>
+      <ul>
+        
+      </ul>
+      <div className="btn"></div>
+    </nav>
+  )
+}
+
+export default Navbar
